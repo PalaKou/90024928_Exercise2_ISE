747451f55a0df193f5a5f73a3544e4cf2a44fefe
PalaKou <alexanderalvin21@gmail.com>
2023-01-29 16:25:43 -0800

    Q4

--- Q4.py
+++ Q4.py
@@ -1,28 +1,15 @@
-'''
-This program implements the Pocket Beast game. The game is played by two players.
-'''
-
-
 import random
 
 def create_beasts():
     '''
     This function creates the beasts for the players.
     '''
-    global player_one_health
-    global player_two_health   
-    global player_one_beast
-    global player_two_beast 
-      
-    player_one_health = 50
-    player_two_health = 50
-
     player_one_beast = input("Player 1, choose your beast: ")
     player_two_beast = input("Player 2, choose your beast: ")
 
     print("Player 1's beast is a " + player_one_beast)
     print("Player 2's beast is a " + player_two_beast)
-
+    return player_one_beast, player_two_beast
 
 def attack(dice_type, dice_number, is_defending):
     '''
@@ -47,9 +34,9 @@ def print_attack_message(player_one_beast, player_one_attack, player_two_beast, 
     elif current_player == 2:
         print(f"{player_two_beast} attacks {player_one_beast} for {player_two_attack} damage")
 
-def play_game():
-    global player_one_health
-    global player_two_health
+def play_game(player_one_beast, player_two_beast):
+    player_one_health = 50
+    player_two_health = 50
     while player_one_health > 0 and player_two_health > 0:
         print(f"{player_one_beast}'s health: {player_one_health}")
         print(f"{player_two_beast}'s health: {player_two_health}")
@@ -70,10 +57,10 @@ def play_game():
         print_attack_message(player_one_beast, player_one_attack, player_two_beast, player_two_attack, 1)
         print_attack_message(player_one_beast, player_one_attack, player_two_beast, player_two_attack, 2)
 
-def print_winner():
-    '''
-    This function prints the winner of the game
-    '''
+
+    return player_one_health, player_two_health
+
+def print_winner(player_one_health, player_two_health, player_one_beast, player_two_beast):
     if player_one_health <= 0:
         print(f"{player_two_beast} wins!")
     else:
@@ -82,5 +69,5 @@ def print_winner():
 
 if __name__ == "__main__":
     create_beasts()
-    play_game()
-    print_winner()
+    play_game(player_one_beast, player_two_beast)
+    print_winner(player_one_health, player_two_health, player_one_beast, player_two_beast)
